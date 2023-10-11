a6a263be1efd707b78b9e4d75792bc432f7909b3
Aryan Kohli <aryankohli0805@gmail.com>
2023-10-11 20:00:46 +0530

    wait

---
+++ wait.js
@@ -0,0 +1,10 @@
+// function createBubble() {
+//     const bubble = document.createElement("div");
+//     bubble.className = "bubble";
+//     bubble.style.left = `${Math.random() * 100}%`;
+//     bubble.style.animationDuration = `${4 + Math.random() * 4}s`; // Randomize bubble rising speed
+//     document.querySelector('.background').appendChild(bubble);
+// }
+
+// // Create bubbles at random intervals
+// setInterval(createBubble, 1000 + Math.random() * 3000);
